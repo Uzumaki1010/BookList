8ae8a96e8b8007edd0949d2e6634661580c329a0
Uzumaki1010 <13616919952@163.com>
2019-10-12 18:19:05 +0800

    完成了定制界面，但布局文件存在问题

--- app/src/main/java/com/casper/testdrivendevelopment/BookListMainActivity.java
+++ app/src/main/java/com/casper/testdrivendevelopment/BookListMainActivity.java
@@ -1,24 +1,60 @@
 package com.casper.testdrivendevelopment;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookListMainActivity extends AppCompatActivity {
 
     ListView listViewBooks;
-    String []bookNames={"Android开发","xin","创新工程实践"};
+    private List<Book> listBooks=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list_main);
 
+        init();
         listViewBooks=this.findViewById(R.id.list_view_books);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                BookListMainActivity.this, android.R.layout.simple_list_item_1, bookNames);
+        BookAdapter adapter = new BookAdapter(BookListMainActivity.this, android.R.layout.,listBooks);
         listViewBooks.setAdapter(adapter);
+    }
+
+    private void init() {
+        listBooks.add(new Book("软件项目管理案例教程（第4版）",R.drawable.book_2));
+        listBooks.add(new Book("创新工程实践",R.drawable.book_no_name));
+        listBooks.add(new Book("信息安全数学基础（第2版）",R.drawable.book_2));
+    }
+
+    class BookAdapter extends ArrayAdapter<Book> {
+
+        private int resourceId;
+
+        public BookAdapter(Context context, int resource, List<Book> objects) {
+            super(context, resource, objects);
+            resourceId = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Book book = getItem(position);//获取当前项的实例
+            View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            ((ImageView) view.findViewById(R.id.image_view_book_cover)).setImageResource(book.getCoverResourceID());
+            ((TextView) view.findViewById(R.id.text_view_book_title)).setText(book.getTitle());
+            return view;
+        }
     }
 }
